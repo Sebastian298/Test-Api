449f4b2e5368c8667fdb617bcb4d3f6ba4dcf82d
Jaime Tenorio <jaime.tenorio@welldex.com>
2022-12-21 14:23:30 -0600

    initial structure for language switching in responses

--- Test-Api/Helpers/MessageErrorBuilder.cs
+++ Test-Api/Helpers/MessageErrorBuilder.cs
@@ -4,13 +4,6 @@ namespace Test_Api.Helpers
 {
 				public class MessageErrorBuilder<T>
 				{
-								//public GenericResponse<T> GetGenericErrorResponse(int statusCode,string controllerName,string methodName,string messageName,string type,Exception ex,string innerExceptionMessage = null)
-								//{
-								//				GenericResponse<T> genericResponse = new GenericResponse<T>();
-
-												
-								//}
-
-
+							
 				}
 }
---
+++ Test-Api/Utils/MessageSetting.cs
@@ -0,0 +1,27 @@
+﻿namespace Test_Api.Utils
+{
+				public class MessageSetting
+				{
+								enum MessageTypes
+								{
+												danger,
+												warning,
+												success,
+												info,
+												primary,
+												secondary,
+												dark,
+												light
+								}
+
+								enum HttpStatusCodes
+								{
+												Ok = 200,
+												BadRequest = 400,
+												Unauthorized = 401,
+												Forbidden = 403,
+												NotFound = 404,
+												InternalServerError=500
+								}
+				}
+}
